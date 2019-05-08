e0c0072ab2b397e4db9c25ee0cb9bec84483f9dd
Gabriel Haddad Campos <dti250@localiza.com>
2019-05-07 22:32:54 -0300

    Migra metodo listAll para o arquivo principal e encapsula config em metodo

--- main.cpp
+++ main.cpp
@@ -6,170 +6,214 @@
 
 using namespace std;
 
+void config (string caminhoComando);
 void importarArquivo(string caminhoComando, string caminhoArquivoImport);
 void exportarArquivo(string caminhoComando, string caminhoArquivoExport, string caminhoDiretorioExport);
+void listAll(string caminhoComando);
 
-//todo: incluir comentarios
 int main(int argc, char **argv)
 {
     cout << "Bem vindo ao Mymfs!" << endl;
-    cout << "Numero de argumentos: " << argc << endl;
 
-    if(argc >= 2){
+    //Deve possuir no minimo 3 argumentos (nome do programa - passado automaticamente,
+    //caminho da unidade X, comando a ser executado)
+    if(argc >= 3)
+    {
         string caminhoComando = argv[1]; //Caminho de onde o Mymfs deve ser executado
         string comando = argv[2];        //Comando do Mymfs que deve ser executado
 
         cout << "Comando solicitado: " << comando << endl;
 
-        if(comando == "config"){
-            string caminhoConfig = caminhoComando + "\\config.txt";
-            cout << "Caminho do arquivo Config: " << caminhoConfig << endl;
-
-            //TODO: encapsular em funcao
-            std::ifstream arquivoConfigExiste( caminhoConfig );
-
-            if(arquivoConfigExiste.good()){             //Verifica se o arquivo Config ja existe no caminho especificado
-                cout << "O Mymfs ja esta configurado nesta unidade." << endl;
-                arquivoConfigExiste.close();
-            }
-            else {
-                arquivoConfigExiste.close();
-                std::ofstream arquivoConfig( caminhoConfig );       //Cria o arquivo config, configurando o Mymfs na unidade especificada
-                arquivoConfig.close();
-                cout << "O Mymfs foi configurado nesta unidade com sucesso." << endl;
-            }
+        if(comando == "config")
+        {
+            config(caminhoComando);
         }
 
-        else if(comando == "import"){
+        else if(comando == "import")
+        {
             string caminhoArquivoImport = argv[3];    //Caminho do arquivo a ser importado para o diretório especficiado
 
             importarArquivo(caminhoComando, caminhoArquivoImport);
         }
 
-        else if(comando == "export"){
+        else if(comando == "export")
+        {
             string caminhoArquivoExport = argv[3];  //Caminho do arquivo a ser exportado para o diretório especficiado
             string caminhoDiretorioExport = argv[4];  //Caminho do diretório para onde o arquivo deve ser exportado
 
             exportarArquivo(caminhoComando, caminhoArquivoExport, caminhoDiretorioExport);
         }
+        else if(comando == "listall")
+        {
+            listAll(caminhoComando);
+        }
     }
-    else{
+    else
+    {
         cout << "Por favor, informe os argumentos necessarios" << endl;
     }
 
     return 0;
 }
 
-void importarArquivo(string caminhoComando, string caminhoArquivoImport){
-    streampos begin, end, pos;
-    std::ifstream arqConfigExiste (caminhoComando + "/config.txt");
-    if(arqConfigExiste.good() && !caminhoArquivoImport.empty()){
-        std::ifstream infile (caminhoArquivoImport, std::ifstream::binary);
-        begin = infile.tellg();
-        cout << "begin is: " << begin << " \n";
-        infile.seekg (0, ios::end);
-        end = infile.tellg();
-        cout << "end is: " << end << " \n";
-        infile.seekg (0);
-        long sizeMax = 512000;
+void config (string caminhoComando){
+    string caminhoConfig = caminhoComando + "/config.txt";
+    cout << "Caminho do arquivo Config: " << caminhoConfig << endl;
 
-        cout << "size do arquivo passado is: " << (end-begin) << " bytes.\n";
+    std::ifstream arquivoConfigExiste( caminhoConfig );
 
-        int numArquivos = ceil((end-begin)/512000.0); // saber quantos arquivos de 500 KB ou menos serão criados
-        cout << "num arquivos: " << numArquivos<< " arquivos. Considerando tamanho máx de 500 KB por arquivo. \n";
-
-        //dividr arquivo passado em arquivos menores de no máx 500 KB
-        char* buffer;
-        infile.seekg(0);
-
-        string nomeDiretorio = caminhoArquivoImport.substr(0, caminhoArquivoImport.find("."));
-        cout << "Nome Diretorio: " + nomeDiretorio << endl;
-        string caminhoDiretorioString = caminhoComando + "/" + nomeDiretorio;
-        char * caminhoDiretorio = new char [caminhoDiretorioString.length()+1];
-        strcpy(caminhoDiretorio, caminhoDiretorioString.c_str());
-
-        cout << "caminho diretorio: " << caminhoDiretorio << endl;
-
-        int erro = CreateDirectory(caminhoDiretorio, NULL);
-
-        if(erro != 0){
-            for(int i=0; i<numArquivos; i++) {
-                auto s = std::to_string(i);
-                cout << "\nArquivo dividido: " << caminhoDiretorioString + "/" + s + ".txt" << " \n";
-
-                std::ofstream outfile (caminhoDiretorioString + "/" + s + ".txt",std::ofstream::binary);
-
-                if(i!= numArquivos-1) {
-                    infile.seekg(pos);
-                    cout << "pos antes is: " << pos << " \n";
-                    buffer = new char[sizeMax];
-                    infile.read (buffer,sizeMax);
-                    pos = infile.tellg();
-                    cout << "pos antes is: " << pos << " \n";
-                    outfile.write (buffer,sizeMax);
-                    delete[] buffer;
-                }
-                else {
-                    infile.seekg(pos);
-                    cout << "pos antes is: " << pos << " \n";
-                    int finalSize = (end-pos);
-                    cout << "finalSize: " << finalSize << " \n";
-                    buffer = new char[finalSize];
-                    infile.read (buffer,finalSize);
-                    pos = infile.tellg();
-                    cout << "pos depois is: " << pos << " \n";
-                    outfile.write (buffer,finalSize);
-                    delete[] buffer;
-                }
-                outfile.close();
-            }
-
-            std::ofstream arqConfig (caminhoComando + "/config.txt", std::ios_base::app | std::ios_base::out);
-            string linhaConfig = nomeDiretorio + ";" + std::to_string(numArquivos) + "\n";
-            arqConfig << linhaConfig;
-            arqConfig.close();
-            cout << "O arquivo foi importado para o Mymfs com sucesso." << endl;
-        }
-        else {
-            cout << "Ocorreu um erro! Um arquivo com esse nome ja existe no Mymfs." << endl;
-        }
-
-        infile.close();
+    if(arquivoConfigExiste.good())              //Verifica se o arquivo Config ja existe no caminho especificado
+    {
+        cout << "O Mymfs ja esta configurado nesta unidade." << endl;
+        arquivoConfigExiste.close();
     }
-    else {
-         cout << "Arquivo nao importado no Mymfs pois o caminho informado esta vazio ou ambiente ainda nao foi configurado." << endl;
+    else
+    {
+        arquivoConfigExiste.close();
+        std::ofstream arquivoConfig( caminhoConfig );       //Cria o arquivo config, configurando o Mymfs na unidade especificada
+        arquivoConfig.close();
+        cout << "O Mymfs foi configurado nesta unidade com sucesso." << endl;
     }
 }
 
-void exportarArquivo(string caminhoComando, string caminhoArquivoExport, string caminhoDiretorioExport){
-//Dar merge
+void importarArquivo(string caminhoComando, string caminhoArquivoImport)
+{
+    streampos begin, end, pos;
     std::ifstream arqConfigExiste (caminhoComando + "/config.txt");
-    if(arqConfigExiste.good() && !caminhoArquivoExport.empty()&& !caminhoDiretorioExport.empty()){
+    if(arqConfigExiste.good())
+    {
+        arqConfigExiste.seekg (0);
+        arqConfigExiste.seekg (0, ios::end);
+        end = arqConfigExiste.tellg();
+        arqConfigExiste.close();
+    }
+    if(end<50000)
+    {
+        if(!caminhoArquivoImport.empty())
+        {
+            std::ifstream infile (caminhoArquivoImport, std::ifstream::binary);
+            begin = infile.tellg();
+            cout << "begin is: " << begin << " \n";
+            infile.seekg (0, ios::end);
+            end = infile.tellg();
+            cout << "end is: " << end << " \n";
+            infile.seekg (0);
+            long sizeMax = 512000;
+
+            cout << "size do arquivo passado is: " << (end-begin) << " bytes.\n";
+
+            int numArquivos = ceil((end-begin)/512000.0); // saber quantos arquivos de 500 KB ou menos serão criados
+            cout << "num arquivos: " << numArquivos<< " arquivos. Considerando tamanho max de 500 KB por arquivo. \n";
+
+            //dividr arquivo passado em arquivos menores de no máx 500 KB
+            char* buffer;
+            infile.seekg(0);
+
+            string nomeDiretorio = caminhoArquivoImport.substr(0, caminhoArquivoImport.find("."));
+            cout << "Nome Diretorio: " + nomeDiretorio << endl;
+            string caminhoDiretorioString = caminhoComando + "/" + nomeDiretorio;
+            char * caminhoDiretorio = new char [caminhoDiretorioString.length()+1];
+            strcpy(caminhoDiretorio, caminhoDiretorioString.c_str());
+
+            cout << "caminho diretorio: " << caminhoDiretorio << endl;
+
+            int erro = CreateDirectory(caminhoDiretorio, NULL);
+
+            if(erro != 0)
+            {
+                for(int i=0; i<numArquivos; i++)
+                {
+                    auto s = std::to_string(i);
+                    cout << "\nArquivo dividido: " << caminhoDiretorioString + "/" + s + ".txt" << " \n";
+
+                    std::ofstream outfile (caminhoDiretorioString + "/" + s + ".txt",std::ofstream::binary);
+
+                    if(i!= numArquivos-1)
+                    {
+                        infile.seekg(pos);
+                        cout << "pos antes is: " << pos << " \n";
+                        buffer = new char[sizeMax];
+                        infile.read (buffer,sizeMax);
+                        pos = infile.tellg();
+                        cout << "pos antes is: " << pos << " \n";
+                        outfile.write (buffer,sizeMax);
+                        delete[] buffer;
+                    }
+                    else
+                    {
+                        infile.seekg(pos);
+                        cout << "pos antes is: " << pos << " \n";
+                        int finalSize = (end-pos);
+                        cout << "finalSize: " << finalSize << " \n";
+                        buffer = new char[finalSize];
+                        infile.read (buffer,finalSize);
+                        pos = infile.tellg();
+                        cout << "pos depois is: " << pos << " \n";
+                        outfile.write (buffer,finalSize);
+                        delete[] buffer;
+                    }
+                    outfile.close();
+                }
+
+                std::ofstream arqConfig (caminhoComando + "/config.txt", std::ios_base::app | std::ios_base::out);
+                string linhaConfig = nomeDiretorio + ";" + std::to_string(numArquivos) + "\n";
+                arqConfig << linhaConfig;
+                arqConfig.close();
+                cout << "O arquivo foi importado para o Mymfs com sucesso." << endl;
+            }
+            else
+            {
+                cout << "Ocorreu um erro! Um arquivo com esse nome ja existe no Mymfs." << endl;
+            }
+
+            infile.close();
+        }
+        else
+        {
+            cout << "Arquivo nao importado no Mymfs pois o caminho informado esta vazio ou ambiente ainda nao foi configurado." << endl;
+        }
+    }
+    else
+        cout << "Nao foi possivel importar o arquivo. Arquivo config.txt está cheio - Mymfs." << endl;
+}
+
+void exportarArquivo(string caminhoComando, string caminhoArquivoExport, string caminhoDiretorioExport)
+{
+//Dar merge
+    std::ifstream arqConfigExiste(caminhoComando + "/config.txt");
+    std::ifstream arqExportExiste(caminhoArquivoExport);
+
+    if(arqConfigExiste.good() &&  arqExportExiste.good() && !caminhoArquivoExport.empty()&& !caminhoDiretorioExport.empty())
+    {
+        arqExportExiste.close();
         string nomeDiretorioBuscado = caminhoArquivoExport.substr(0, caminhoArquivoExport.find("."));
         string nomeDiretorioEncontrado;
         string qtdArquivosEncontrado;
-        do{
 
+        do
+        {
             string linhaConfig;
             std::getline(arqConfigExiste, linhaConfig);
             nomeDiretorioEncontrado = linhaConfig.substr(0, linhaConfig.find(";"));
             qtdArquivosEncontrado = linhaConfig.substr(linhaConfig.find(";") + 1, (linhaConfig.size()-linhaConfig.find(";")));
-            //cout << nomeDiretorioEncontrado << endl;
-            //cout <<"qtd: " + qtdArquivosEncontrado << endl;
-
-        } while (strcmp(nomeDiretorioEncontrado.c_str(), nomeDiretorioBuscado.c_str()) != 0 &&
-                 !nomeDiretorioEncontrado.empty());
+        }
+        while (strcmp(nomeDiretorioEncontrado.c_str(), nomeDiretorioBuscado.c_str()) != 0 &&
+                !arqConfigExiste.eof() && !nomeDiretorioEncontrado.empty() ); //EDITADO TALITA
 
         if(strcmp(nomeDiretorioEncontrado.c_str(), nomeDiretorioBuscado.c_str()) == 0 &&
-                 !nomeDiretorioEncontrado.empty() && !qtdArquivosEncontrado.empty()){
+                !nomeDiretorioEncontrado.empty() && !qtdArquivosEncontrado.empty())
+        {
             int numArquivos = stoi(qtdArquivosEncontrado);
             cout << "Arquivo encontrado! Exportando..." << endl;
-            std::ifstream arquivoJaExiste(caminhoDiretorioExport + "/" + nomeDiretorioEncontrado +".txt" );
-            if(arquivoJaExiste.good()){
+            std::ifstream arquivoJaExiste(caminhoDiretorioExport); //EDITADO TALITA
+            if(arquivoJaExiste.good())
+            {
                 arquivoJaExiste.close();
-                cout << "O arquivo a ser exportado ja existe na pasta destino (" << caminhoDiretorioExport + "/" + nomeDiretorioEncontrado + ".txt" << "), por favor indique outro destino." << endl;
-            } else {
-                std::ofstream combined_file(caminhoDiretorioExport + "/" + nomeDiretorioEncontrado +".txt" );
+                cout << "O arquivo a ser exportado ja existe na pasta destino (" << caminhoDiretorioExport << "), por favor indique outro destino." << endl; //EDITADO TALITA
+            }
+            else
+            {
+                std::ofstream combined_file(caminhoDiretorioExport); //EDITADO TALITA
                 for(int i=0; i<numArquivos; i++)
                 {
                     auto s = std::to_string(i);
@@ -179,7 +223,8 @@ void exportarArquivo(string caminhoComando, string caminhoArquivoExport, string 
                     if(srce_file)
                     {
                         combined_file << srce_file.rdbuf() ;
-                        if(combined_file){
+                        if(combined_file)
+                        {
                             std::cout << "Arquivo " + s + " concatenado\n" ;
                         }
                     }
@@ -193,11 +238,49 @@ void exportarArquivo(string caminhoComando, string caminhoArquivoExport, string 
                 cout << "O arquivo foi exportado do Mymfs com sucesso." << endl;
             }
 
-        } else {
+        }
+        else
+        {
             cout << "O arquivo nao foi encontrado no Mymfs, portanto nao foi exportado" << endl;
         }
     }
-    else {
-         cout << "Arquivo nao exportado do Mymfs pois o caminho informado esta vazio ou ambiente ainda nao foi configurado." << endl;
+    else
+    {
+        arqExportExiste.close();
+        cout << "Arquivo nao exportado do Mymfs pois o caminho informado esta vazio ou ambiente ainda nao foi configurado." << endl;
+    }
+}
+
+void listAll(string caminhoComando)  //EDITADO TALITA
+{
+//Dar merge
+    std::ifstream arqConfigExiste(caminhoComando + "/config.txt");
+    if(arqConfigExiste.good())
+    {
+        string nomeDiretorioEncontrado = "x";
+        string qtdArquivosEncontrado;
+        string linhaConfig;
+        std::getline(arqConfigExiste, linhaConfig);
+        if(linhaConfig.length() > 0)
+        {
+            cout << "Arquivos encontrados: \n" << endl;
+            nomeDiretorioEncontrado = linhaConfig.substr(0, linhaConfig.find(";"));
+            while (!arqConfigExiste.eof() && !nomeDiretorioEncontrado.empty())
+            {
+                cout << nomeDiretorioEncontrado + ".txt" << endl;
+                string linhaConfig;
+                std::getline(arqConfigExiste, linhaConfig);
+                nomeDiretorioEncontrado = linhaConfig.substr(0, linhaConfig.find(";"));
+            }
+            cout << "\nFim da lista de arquivos encontrados. \n" << endl;
+        }
+        else
+        {
+            cout << "Nao ha arquivos salvos pelo Mymfs!" << endl;
+        }
+    }
+    else
+    {
+        cout << "Arquivos nao listados do Mymfs pois o caminho informado esta vazio ou ambiente ainda nao foi configurado." << endl;
     }
 }
