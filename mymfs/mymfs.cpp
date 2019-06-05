c8f70463ae6fc4671acd6c4507f8d51c480a36b8
Talita Arantes <40677715+talitaam@users.noreply.github.com>
2019-06-05 03:42:23 -0300

    Adição de verificação antes de chamar o metodos

--- mymfs/mymfs.cpp
+++ mymfs/mymfs.cpp
@@ -20,51 +20,76 @@ int main(int argc, char **argv) {
 		}
 
 		else if (comando == "import") {
-			string caminhoArquivoImport = argv[3];    //Caminho do arquivo a ser importado para o diretório especficiado
-			
-			importarArquivo(caminhoComando, caminhoArquivoImport);
-		}
-		
-		else if (comando == "export") {
-			string caminhoArquivoExport = argv[3];    //Caminho do arquivo a ser exportado para o diretório especficiado
-			string caminhoDiretorioExport = argv[4];  //Caminho do diretório para onde o arquivo deve ser exportado
+			if (argc == 4) {
+				string caminhoArquivoImport = argv[3];    //Caminho do arquivo a ser importado para o diretÃ³rio especficiado
 
-			exportarArquivo(caminhoComando, caminhoArquivoExport, caminhoDiretorioExport);
+				importarArquivo(caminhoComando, caminhoArquivoImport);
+			}
+			else
+				cout << "Comando import incorreto." << endl;
 		}
-		
+
+		else if (comando == "export") {
+			if (argc == 5) {
+				string caminhoArquivoExport = argv[3];    //Caminho do arquivo a ser exportado para o diretÃ³rio especficiado
+				string caminhoDiretorioExport = argv[4];  //Caminho do diretÃ³rio para onde o arquivo deve ser exportado
+
+				exportarArquivo(caminhoComando, caminhoArquivoExport, caminhoDiretorioExport);
+			}
+			else
+				cout << "Comando export incorreto." << endl;
+		}
+
 		else if (comando == "listall") {
 			listAll(caminhoComando);
 		}
-		
+
 		else if (comando == "remove") {
-			string caminhoArquivoRemove = argv[3];    //Caminho do arquivo a ser removido
-			
-			remove(caminhoComando, caminhoArquivoRemove);
+			if (argc == 4) {
+				string caminhoArquivoRemove = argv[3];    //Caminho do arquivo a ser removido
+
+				remove(caminhoComando, caminhoArquivoRemove);
+			}
+			else
+				cout << "Comando remove incorreto." << endl;
 		}
-		
+
 		else if (comando == "removeall") {
 			removeAll(caminhoComando);
 		}
-		
+
 		else if (comando == "grep") {
-			string palavra = argv[3];    //Palavra que será procurado no arquivo
-			string caminhoArquivoToRead = argv[4];  //Caminho do arquivo que será pesquisado
-			
-			procuraPalavra(caminhoComando, palavra, caminhoArquivoToRead);
+			if (argc == 5) {
+				string palavra = argv[3];    //Palavra que serÃ¡ procurado no arquivo
+				string caminhoArquivoToRead = argv[4];  //Caminho do arquivo que serÃ¡ pesquisado
+
+				procuraPalavra(caminhoComando, palavra, caminhoArquivoToRead);
+			}
+			else
+				cout << "Comando grep incorreto." << endl;
 		}
 
 		else if (comando == "head100") {
-			string caminhoArquivoToRead = argv[3];  //Caminho do arquivo que será lido
+			if (argc == 4) {
+				string caminhoArquivoToRead = argv[3];  //Caminho do arquivo que serÃ¡ lido
 
-			primeiras100Linhas(caminhoComando, caminhoArquivoToRead);
+				primeiras100Linhas(caminhoComando, caminhoArquivoToRead);
+			}
+			else
+				cout << "Comando head100 incorreto." << endl;
 		}
 
 		else if (comando == "tail100") {
-			string caminhoArquivoToRead = argv[3];  //Caminho do arquivo que será lido
+			if (argc == 4) {
+				string caminhoArquivoToRead = argv[3];  //Caminho do arquivo que serÃ¡ lido
 
-			ultimas100Linhas(caminhoComando, caminhoArquivoToRead);
+				ultimas100Linhas(caminhoComando, caminhoArquivoToRead);
+			}
+			else
+				cout << "Comando tail100 incorreto." << endl;
 		}
-
+		else
+			cout << "Comando invalido." << endl;
 	}
 	else {
 		cout << "Por favor, informe os argumentos necessarios" << endl;
